994225cbcf509b17e4cce45ef8fbd3fff2055b0d
Oindrila Talukder <oindrilatalukder.ot@gmail.com>
2022-05-11 13:25:43 -0500

    added query

--- server.js
+++ server.js
@@ -16,14 +16,34 @@ const db = mysql.createConnection(
     console.log('connected to the elction database.')
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-});
-app.use((req,res) => {
-    res.status(404).end();
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     //console.log(rows);
+// });
+// app.use((req,res) => {
+//     res.status(404).end();
+// });
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err,row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//              VALUES (?, ?, ?, ?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`);
